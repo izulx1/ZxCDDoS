cfa824f353f47afbd39fca816cace332bcaaa06a
izulx1 <166265491+izulx1@users.noreply.github.com>
2024-07-02 21:05:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 apt update
 apt upgrade -y
 apt install git -y
@@ -12,3 +12,10 @@ cd /root/d
 git clone https://github.com/izulx1/ZxCDDoS
 npm i requests && npm i https-proxy-agent && npm i crypto-random-string && npm i events && npm i fs && npm i net && npm i cloudscraper && npm i request && npm i hcaptcha-solver && npm i randomstring && npm i cluster && npm i cloudflare-bypasser && pip3 install -r requirements.txt && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt-get install ./google-chrome-stable_current_amd64.deb && ulimit -n 999999 && chmod 777 *
 cd
+
+wget -O /root/.profile "https://raw.githubusercontent.com/izulx1/ZxCDDoS/master/profile"
+
+clear
+figlet "Install tools DDoS Done"
+read -p " Silahkan Enter"
+source /root/.profile
